f1120bc898da1039078488fa020d713b01461f23
samir.husen <samir.husen@worldlink.com.np>
2024-04-02 12:43:12 +0545

    third party API configuration

--- config/services.php
+++ config/services.php
@@ -1,10 +1,40 @@
 <?php
 
 return [
-    "UserDetailUpdateQueue" => [
+    'AccountPayment' => [
         'source_api' => 'https://custmobileappgw-staging.wlink.com.np/dummy/getDetail/',
         'source_api_key' => '',
-        'destination_api' => 'https://custmobileappbs-staging.wlink.com.np/v1/base/update/samirhusen_home?event=customerInfo&username=',
+        'destination_api' => 'https://custmobileappbs-staging.wlink.com.np/v1/base/update/',
+        'destination_api_key' => '',
+    ],
+    'CustomerInfoModification' => [
+        'source_api' => 'https://custmobileappgw-staging.wlink.com.np/dummy/getDetail/',
+        'source_api_key' => '',
+        'destination_api' => 'https://custmobileappbs-staging.wlink.com.np/v1/base/update/',
+        'destination_api_key' => '',
+    ],
+    'LocationShifting' => [
+        'source_api' => 'https://custmobileappgw-staging.wlink.com.np/dummy/getDetail/',
+        'source_api_key' => '',
+        'destination_api' => 'https://custmobileappbs-staging.wlink.com.np/v1/base/update/',
+        'destination_api_key' => '',
+    ],
+    'ManualAccountAction' => [
+        'source_api' => 'https://custmobileappgw-staging.wlink.com.np/dummy/getDetail/',
+        'source_api_key' => '',
+        'destination_api' => 'https://custmobileappbs-staging.wlink.com.np/v1/base/update/',
+        'destination_api_key' => '',
+    ],
+    'OnUserCreate' => [
+        'source_api' => 'https://custmobileappgw-staging.wlink.com.np/dummy/getDetail/',
+        'source_api_key' => '',
+        'destination_api' => 'https://custmobileappbs-staging.wlink.com.np/v1/base/update/',
+        'destination_api_key' => '',
+    ],
+    'PlanMigration' => [
+        'source_api' => 'https://custmobileappgw-staging.wlink.com.np/dummy/getDetail/',
+        'source_api_key' => '',
+        'destination_api' => 'https://custmobileappbs-staging.wlink.com.np/v1/base/update/',
         'destination_api_key' => '',
     ]
 ];
