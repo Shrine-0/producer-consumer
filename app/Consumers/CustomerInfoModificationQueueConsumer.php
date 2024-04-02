fb721394241fea7fb2dd15b6d14ee8967315992b
samir.husen <samir.husen@worldlink.com.np>
2024-04-02 12:40:45 +0545

    CustomerInfoModification consumer

---
+++ app/Consumers/CustomerInfoModificationQueueConsumer.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Consumers;
+
+class CustomerInfoModificationQueueConsumer extends QueueConsumer
+{
+    protected function getEventName(): string
+    {
+        return "CustomerInfoModification";
+    }
+
+    protected function transformPayload($data)
+    {
+        $data = $data['response'][0];
+        $data['client_name'] = $data['username'] . rand();
+        return $data;
+    }
+
+    protected function getHttpMethod(): string
+    {
+        return "PATCH";
+    }
+
+    protected function sourceApiQueryParams(): string
+    {
+        return "";
+    }
+
+    protected function destinationApiQueryParams(): string
+    {
+        return "?event=customerInfo";
+    }
+}
