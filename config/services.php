9dc5e815e30fba6d7a335cab3bb50e9a5be74240
samir.husen <samir.husen@worldlink.com.np>
2024-04-03 11:53:44 +0545

    third party api services modified

--- config/services.php
+++ config/services.php
@@ -2,38 +2,26 @@
 
 return [
     'AccountPayment' => [
-        'source_api' => 'https://custmobileappgw-staging.wlink.com.np/dummy/getDetail/',
-        'source_api_key' => '',
         'destination_api' => 'https://custmobileappbs-staging.wlink.com.np/v1/base/update/',
         'destination_api_key' => '',
     ],
     'CustomerInfoModification' => [
-        'source_api' => 'https://custmobileappgw-staging.wlink.com.np/dummy/getDetail/',
-        'source_api_key' => '',
         'destination_api' => 'https://custmobileappbs-staging.wlink.com.np/v1/base/update/',
         'destination_api_key' => '',
     ],
-    'LocationShifting' => [
-        'source_api' => 'https://custmobileappgw-staging.wlink.com.np/dummy/getDetail/',
-        'source_api_key' => '',
-        'destination_api' => 'https://custmobileappbs-staging.wlink.com.np/v1/base/update/',
-        'destination_api_key' => '',
-    ],
-    'ManualAccountAction' => [
-        'source_api' => 'https://custmobileappgw-staging.wlink.com.np/dummy/getDetail/',
-        'source_api_key' => '',
-        'destination_api' => 'https://custmobileappbs-staging.wlink.com.np/v1/base/update/',
-        'destination_api_key' => '',
-    ],
+    // 'LocationShifting' => [
+    //     'destination_api' => 'https://custmobileappbs-staging.wlink.com.np/v1/base/update/',
+    //     'destination_api_key' => '',
+    // ],
+    // 'ManualAccountAction' => [
+    //     'destination_api' => 'https://custmobileappbs-staging.wlink.com.np/v1/base/update/',
+    //     'destination_api_key' => '',
+    // ],
     'OnUserCreate' => [
-        'source_api' => 'https://custmobileappgw-staging.wlink.com.np/dummy/getDetail/',
-        'source_api_key' => '',
         'destination_api' => 'https://custmobileappbs-staging.wlink.com.np/v1/base/update/',
         'destination_api_key' => '',
     ],
     'PlanMigration' => [
-        'source_api' => 'https://custmobileappgw-staging.wlink.com.np/dummy/getDetail/',
-        'source_api_key' => '',
         'destination_api' => 'https://custmobileappbs-staging.wlink.com.np/v1/base/update/',
         'destination_api_key' => '',
     ]
