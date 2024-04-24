945c1a9328b13915c725a57fc56395de8525e61a
samir.husen <samir.husen@worldlink.com.np>
2024-04-24 17:16:02 +0545

    master command stopped for production

--- app/Console/Commands/MasterCommand.php
+++ app/Console/Commands/MasterCommand.php
@@ -5,6 +5,7 @@ namespace App\Console\Commands;
 use App\Helpers\RedisHelper;
 use App\Services\EbillService;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
 
 class MasterCommand extends Command
 {
@@ -20,9 +21,11 @@ class MasterCommand extends Command
         $supportZoneData = $eBillService->getSupportZoneCustomerCountDetails();
 
         foreach ($supportZoneData as $supportZone => $count) {
+            $this->logger('STARTED', "Redis cache store total customer count $count", $supportZone);
             $set = $redis->setKey("-supportzone-$supportZone", $count);
-            // $this->info("Redis store supportzone-$supportZone : Count $count -> $set");
+            $this->logger('FINISHED', "Redis cache store total customer count $count with response $set", $supportZone);
         }
+        return;
 
         $listOfBranches = $eBillService->getListOfAllBranches();
 
@@ -30,5 +33,15 @@ class MasterCommand extends Command
             exec("php artisan command:sync-support-zone-customers '$key' '$value' >/dev/null 2>&1 &");
 
         $this->info('DONE');
+    }
+
+    public function logger($process, $message, $support_zone = null)
+    {
+        $logData = [
+            'support_zone' => $support_zone,
+            'process' => $process,
+            'message' => strtoupper($message),
+        ];
+        Log::info(json_encode($logData));
     }
 }
