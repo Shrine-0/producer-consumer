455fbeccfd64e958dcdbb6ad4bb5087b085eaa76
samir.husen <samir.husen@worldlink.com.np>
2024-05-02 12:13:38 +0545

    time change

--- routes/console.php
+++ routes/console.php
@@ -3,5 +3,5 @@
 use Illuminate\Support\Facades\Schedule;
 
 Schedule::command('retry:consume')->everyMinute()->withoutOverlapping()->sendOutputTo('/proc/1/fd/1');
-Schedule::command('command:master')->dailyAt('12:10')->withoutOverlapping()->sendOutputTo('/proc/1/fd/1');
+Schedule::command('command:master')->dailyAt('12:20')->withoutOverlapping()->sendOutputTo('/proc/1/fd/1');
 // Schedule::command('command:master')->dailyAt('01:00')->withoutOverlapping()->sendOutputTo('/proc/1/fd/1');
